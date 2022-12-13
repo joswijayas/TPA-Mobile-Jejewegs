14ea0a60ef53e40b7167df0de3463cfe58bf97d7
joswijayas <jos.programmer@gmail.com>
2022-12-13 20:38:52 +0700

    Fix bug
    fixing bug and create review

--- app/src/main/java/edu/bluejack22_1/Jejewegs/LoginActivity.kt
+++ app/src/main/java/edu/bluejack22_1/Jejewegs/LoginActivity.kt
@@ -1,5 +1,6 @@
 package edu.bluejack22_1.Jejewegs
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +8,14 @@ import android.util.Log
 import android.util.Patterns
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Api
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.auth.User
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -20,33 +28,49 @@ class LoginActivity : AppCompatActivity() {
     val db = Firebase.firestore
     private val TAG = "RegisterActivity"
     private lateinit var auth : FirebaseAuth
-
+    private lateinit var googleSigninClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLogin2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        val currUser = auth.currentUser
+
+        if(currUser != null){
+            currUser.displayName?.let { Log.d("aaa", it) }
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("31969072633-tmtv8otf72i0lnsdkprihbsqb2min80j.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSigninClient = GoogleSignIn.getClient(this, gso)
+
+        binding.btnGoogleSignin.setOnClickListener{
+            signIn();
+        }
 
         binding.tvToRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             val email = binding.edtEmailLogin.text.toString()
             val password = binding.edtPasswordLogin.text.toString()
-            if (email.isEmpty()){
+            if (email.isEmpty()) {
                 binding.edtEmailLogin.error = "Email must be filled"
                 binding.edtEmailLogin.requestFocus()
                 return@setOnClickListener
             }
-            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 binding.edtEmailLogin.error = "Email format wrong"
                 binding.edtEmailLogin.requestFocus()
                 return@setOnClickListener
             }
-            if(password.isEmpty()){
+            if (password.isEmpty()) {
                 binding.edtPasswordLogin.error = "Password must be filled"
                 binding.edtPasswordLogin.requestFocus()
                 return@setOnClickListener
@@ -57,6 +81,7 @@ class LoginActivity : AppCompatActivity() {
                     if(task.isSuccessful){
                         val intent = Intent(this , MainActivity::class.java)
                         startActivity(intent)
+                        finish()
                     }else{
                         Toast.makeText(this , "INVALID CREDENTIAL" , Toast.LENGTH_SHORT).show()
                     }
@@ -70,4 +95,56 @@ class LoginActivity : AppCompatActivity() {
             )
         }
     }
+
+    private fun signIn(){
+        val signInIntent = googleSigninClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    companion object {
+        const val RC_SIGN_IN = 1001
+        const val EXTRA_NAME = "EXTRA NAME"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(ContentValues.TAG, "firebaseAuthWithGoogle: " + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }catch (e:ApiException){
+                Log.w(ContentValues.TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this){task->
+                if(task.isSuccessful){
+                    Log.d(ContentValues.TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI()
+                }
+                else{
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI()
+                }
+        }
+    }
+
+    private fun updateUI(){
+        val user = auth.currentUser
+        if(user != null){
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra(EXTRA_NAME, user.displayName)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
 }
